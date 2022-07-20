0aa70c9a9cd53dce906ed1dea824f537bf59b026
mehmet_ege_acican <ege.acican@onbiron.com.tr>
2022-07-20 13:23:45 +0300

    MongoDB Connection has been established

--- Server/server.js
+++ Server/server.js
@@ -1,16 +1,28 @@
 //CONFIG
 require('dotenv').config();
+
+
 const express = require('express');
 const blogRoutes = require("./routes/blogRoutes");
+const mongoose = require("mongoose");
+
 //Express App
 const app = express();
+
 //MIDDLEWARE
 //MIDDLEWARE FOR REQ.BODY
 app.use(express.json())
 
 //routes
 app.use("/api/v1/blogs/",blogRoutes);
-//Listen for Requests
-app.listen(process.env.PORT,() => {
-    console.log("Listening on Port 4000");
-})
+
+//CONNECT TO DB
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log("Connected to MongoDB Successfully");
+    //Listen for Requests
+    app.listen(process.env.PORT,() => {
+        console.log("Listening on Port 4000");
+    })
+}).catch((err) => {
+    console.log(err);
+});
