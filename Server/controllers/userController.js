3fa4f4fc2410fe9efa9a4ba9262dfe179a0f0b94
Mehmet Ege Acican <mehmetegeacican@192.168.1.89>
2023-05-13 09:06:27 +0300

    Sign up Modal added

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -6,16 +6,24 @@ const User = require('../models/userModel');
  * @param {*} req 
  * @param {*} res 
  */
-const loginUser = async (req,res) => {
-    res.json({msg:"Login"});
+const loginUser = async (req, res) => {
+    res.json({ msg: "Login" });
 }
 /**
  * Sign Up 
  * @param {*} req 
  * @param {*} res 
  */
-const signupUser = async (req,res) => {
-    res.json({msg:"SignUp"});
+const signupUser = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.signup(email, password);
+        res.status(201).json({ email, user });
+    }
+    catch (e) {
+        res.status(400).json({ error: e.msg });
+    }
+
 }
 
 module.exports = {
--- Server/models/userModel.js
+++ Server/models/userModel.js
@@ -1,16 +1,38 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcrypt');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    email:{
-        type:String,
-        required:true,
+    email: {
+        type: String,
+        required: true,
         unique: true
     },
-    password:{
-        type:String,
-        required:true,
+    password: {
+        type: String,
+        required: true,
     }
 })
 
-module.exports = mongoose.model('User',userSchema);
+
+/**
+ * Static function for sign Up
+ * @param {string} email 
+ * @param {*string} password 
+ */
+userSchema.statics.signup = async function (email, password) {
+    //Step 1 -- Check if email exists, if it doesn't, return null value
+    const exists = await this.findOne({ email });
+    if (exists) {
+        throw Error('Email already in use');
+    }
+    //step 2 -- Add salt and Hash
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+    //Step 3 --> Create Document for the user
+    const user = await this.create({ email, password: hash });
+    return user;
+}
+
+module.exports = mongoose.model('User', userSchema);
