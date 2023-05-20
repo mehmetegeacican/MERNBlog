2af3709e89a123a6e14e73f5864809635a190977
Mehmet Ege Acican <mehmetegeacican@192.168.1.60>
2023-05-20 15:40:32 +0300

    auth middleware

--- Server/middleware/requireAuth.js
+++ Server/middleware/requireAuth.js
@@ -6,24 +6,24 @@ const User = require('../models/userModel');
  * @param {*} res response
  * @param {*} next next middleware
  */
-const requireAuth = async (req,res,next) => {
+const requireAuth = async (req, res, next) => {
     //Step 1 --> Verify Auth
     const { authorization } = req.headers;
     //Step 2 --> Check if there is no authorization
-    if(!authorization){
-        return res.status(401).json({error:'Authorization token required'});
+    if (!authorization) {
+        return res.status(401).json({ error: 'Authorization token required' });
     }
     // [Bearer, asdasdaas]
     const token = authorization.split(" ")[1];
     //Step 3 --> Verify token
-    try{
-        const {_id} = jwt.verify(token,process.env.SECRET);
-        req.user = await User.findOne({_id}).select('_id');
+    try {
+        const { _id } = jwt.verify(token, process.env.SECRET);
+        req.user = await User.findOne({ _id }).select('_id');
         next();
     }
-    catch(error){
+    catch (error) {
         console.log(error);
-        res.status(401).error({error:"Request is not authorized"});
+        res.status(401).error({ error: "Request is not authorized" });
     }
 
 }
