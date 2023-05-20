2422007dd066663ce5e19a0601a30f0d70074338
mehmetegeacican <47591567+mehmetegeacican@users.noreply.github.com>
2023-05-20 17:35:03 +0300

    React redirects added (#25)
    Co-authored-by: Mehmet Ege Acican <mehmetegeacican@192.168.1.60>

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import NavBar from "./components/layout/NavBar";
 import Blogs from "./pages/blogs";
 import Footer from "./components/layout/Footer";
@@ -6,19 +6,23 @@ import AddBlog from "./pages/addBlog";
 import BlogDetails from "./pages/blogDetails";
 import SignUp from "./pages/signUp";
 import Login from "./pages/login";
+import { useAuthContext } from "./hooks/useAuthContext";
 
 function App() {
+
+  //Context
+  const { user } = useAuthContext();
   return (
     <div className="App">
       <BrowserRouter>
         <NavBar />
         <section className="hero is-large">
           <Routes>
-            <Route path="/" element={<Blogs />} />
-            <Route path="/create" element={<AddBlog />} />
-            <Route path="/:id" element={<BlogDetails />} />
-            <Route path="/signup" element={<SignUp/>} />
-            <Route path="/login" element={<Login/>} />
+            <Route path="/" element={user ? <Blogs /> : <Navigate to='/login' />} />
+            <Route path="/create" element={user ? <AddBlog /> : <Navigate to='/login' />} />
+            <Route path="/:id" element={user ? <BlogDetails /> : <Navigate to='/login' />} />
+            <Route path="/signup" element={!user ? <SignUp /> : <Navigate to="/" />} />
+            <Route path="/login" element={!user ? <Login /> : <Navigate to="/" />} />
           </Routes>
         </section>
         <section className="hero is-small">
