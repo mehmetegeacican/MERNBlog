20d7444baeab09e7fdec31b473c721ba75e217d9
Mehmet Ege Acican <mehmetegeacican@192.168.1.70>
2023-05-21 09:23:00 +0300

    Blogs have been personalized

--- Server/models/blogModel.js
+++ Server/models/blogModel.js
@@ -20,6 +20,10 @@ const blogSchema = new Schema(
       type: String,
       required: true,
     },
+    user_id : {
+      type: String,
+      required:true
+    }
   },
   { timestamps: true }
 );
